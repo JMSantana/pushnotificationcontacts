51b3718175b788a3bc6eb86abbcd3847f2d352c3
João Marco de Paula Santana <Joo Marco de Paula Santana>
2017-02-21 21:42:08 -0300

    Fix for the onMessageReceived to work properly

--- app/src/main/java/jmsoft/pushnotificationcontacts/service/MessagingService.java
+++ app/src/main/java/jmsoft/pushnotificationcontacts/service/MessagingService.java
@@ -1,7 +1,6 @@
 package jmsoft.pushnotificationcontacts.service;
 
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -18,18 +17,15 @@ public class MessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(Util.getAppTag(), "From: " + remoteMessage.getFrom());
-        Toast.makeText(getApplicationContext(), remoteMessage.getFrom(), Toast.LENGTH_LONG).show();
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(Util.getAppTag(), "Message data payload: " + remoteMessage.getData());
-            Toast.makeText(getApplicationContext(), "Message data payload: " + remoteMessage.getData(), Toast.LENGTH_LONG).show();
         }
 
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(Util.getAppTag(), "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            Toast.makeText(getApplicationContext(), "Message Notification Body: " + remoteMessage.getNotification().getBody(), Toast.LENGTH_LONG).show();
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
